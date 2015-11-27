ce48876837a757ae1519a98bab615b8aef20820a
Andre Fonseca <afonseca@localhost.localdomain>
2015-11-27 16:58:57 +0000

    05 initial commit

---
+++ 05_Trabalho/js/DrawingShape.js
@@ -0,0 +1,199 @@
+function DrawingShape(x,y,color,cnvs)
+{
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.cnvs = cnvs;
+    this.ctx = this.cnvs.getContext("2d");
+
+    this.draw = function() {
+    };
+
+    this.overcheck = function (mx,my) {
+        if ( (mx>=this.x)&&(mx<=(this.x+this.w))&&(my>=this.y)&&(my<=(this.y+this.h))) {
+            return true;
+        }
+        return false;
+    };
+
+    this.distsq = function (x1,y1,x2,y2) {
+        var xd = x1 - x2;
+        var yd = y1 - y2;
+        return ((xd*xd) + (yd*yd) );
+    };
+}
+
+
+function Rect(x,y,w,h,c,cnvs) {
+    this.w = w;
+    this.h = h;
+
+    DrawingShape.call(this,x,y,c,cnvs);
+
+    this.draw = function () {
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.w, this.h);
+    };
+}
+
+Rect.prototype = Object.create(DrawingShape.prototype);
+Rect.prototype.constructor = Rect;
+
+
+function Picture(x,y,w,h,imagename,cnvs) {
+    this.w = w;
+    this.h = h;
+    this.imagename = imagename;
+
+    DrawingShape.call(this,x,y,"white",cnvs);
+
+    this.draw = function() {
+        this.ctx.drawImage(this.imagename,this.x,this.y,this.w,this.h);
+    };
+}
+
+Picture.prototype = Object.create(DrawingShape.prototype);
+Picture.prototype.constructor = Picture;
+
+
+function Oval(x,y,r,hor,ver,c,cnvs) {
+    this.r = r;
+    this.radsq = r*r;
+    this.hor = hor;
+    this.ver = ver;
+
+    DrawingShape.call(this,x,y,c,cnvs);
+
+    this.overcheck = function overoval(mx,my) {
+        var x1 = 0;
+        var y1 = 0;
+        var x2 = (mx-this.x)/this.hor;
+        var y2 = (my-this.y)/this.ver;
+        if (this.distsq(x1,y1,x2,y2)<=(this.radsq) ){
+            return true;
+        }
+        return false;
+    };
+
+    this.draw = function() {
+        this.ctx.save();
+        this.ctx.translate(this.x,this.y);
+        this.ctx.scale(this.hor,this.ver);
+        this.ctx.fillStyle = this.color;
+        this.ctx.beginPath();
+        this.ctx.arc(0,0,this.r,0,2*Math.PI,true);
+        this.ctx.closePath();
+        this.ctx.fill();
+        this.ctx.restore();
+    };
+}
+Oval.prototype = Object.create(DrawingShape.prototype);
+Oval.prototype.constructor = Oval;
+
+
+function Heart(x,y,h,drx,color,cnvs) {
+    this.h = h;
+    this.drx = drx;
+    this.radsq = drx*drx;
+    this.ang = .25*Math.PI;
+
+    DrawingShape.call(this,x,y,color,cnvs);
+
+    var outside = function (x,y,w,h,mx,my) {
+        return ((mx<x) || (mx > (x+w)) || (my < y) || (my > (y+h)));
+    };
+
+    this.draw = function() {
+        var leftctrx = this.x-this.drx;
+        var rightctrx = this.x+this.drx;
+        var cx = rightctrx+this.drx*Math.cos(this.ang);
+        var cy = this.y + this.drx*Math.sin(this.ang);
+        this.ctx.fillStyle = this.color;
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.x,this.y);
+        this.ctx.arc(leftctrx,this.y,this.drx,0,Math.PI-this.ang,true);
+        this.ctx.lineTo(this.x,this.y+this.h);
+        this.ctx.lineTo(cx,cy);
+        this.ctx.arc(rightctrx,this.y,this.drx,this.ang,Math.PI,true);
+        this.ctx.closePath();
+        this.ctx.fill();
+    };
+
+    this.overcheck = function (mx,my) {
+        var leftctrx = this.x-this.drx;
+        var rightctrx = this.x+this.drx;
+        var qx = this.x-2*this.drx;
+        var qy = this.y-this.drx;
+        var qwidth = 4*this.drx;
+        var qheight = this.drx+this.h;
+
+        //quick test if it is in bounding rectangle
+        if (outside(qx,qy,qwidth,qheight,mx,my)) {
+            return false;
+        }
+
+        //compare to two centers
+        if (this.distsq(mx,my,leftctrx,this.y)<this.radsq) return true;
+        if (this.distsq(mx,my,rightctrx,this.y)<this.radsq) return true;
+
+        // if outside of circles AND less than equal to y, return false
+        if (my<=this.y) return false;
+
+        // compare to each slope
+        var x2 = this.x
+        var y2 = this.y + this.h;
+        var m = (this.h)/(2*this.drx);
+
+        // left side
+        if (mx<=this.x) {
+            if (my < (m*(mx-x2)+y2)) {
+                return true;
+            }
+            return false;
+        } else {  //right side
+            m = -m;
+            if (my < (m*(mx-x2)+y2)) {
+                return true;
+            }
+            return false;
+        }
+    };
+}
+Heart.prototype = Object.create(DrawingShape.prototype);
+Heart.prototype.constructor = Heart;
+
+function getSymmetry(coord, size) {
+  return (coord + size) / 2;
+}
+
+function Bear(x, y, width, height, color, canvas) {
+  var xS = getSymmetry(x, width);
+  var yS = getSymmetry(x, height);
+  console.log(x + width/2);
+  console.log("%d, %d, %d, %d", x, y, xS, yS);
+  var square = new Rect(x, y, width, height, color, canvas);
+  var face = new Oval(x + xS, y + yS, width/2, 0.75, 0.75, "white", canvas);
+  var lEye = new Oval((x + width)/2-15, y + width/2, width/4, 0.25, 0.25, "black", canvas);
+  var rEye = new Oval((x + width)/2+15, y + width/2, width/4, 0.25, 0.25, "black", canvas);
+  var iReye;
+  var iLeye;
+  var lEar;
+  var rEar;
+  var iLear;
+  var iRear;
+  var nose;
+  var iNose;
+  var lMouth;
+  var rMouth;
+
+
+  this.draw = function () {
+    square.draw();
+    face.draw();
+    lEye.draw();
+    rEye.draw();
+  };
+}
+
+Bear.prototype = Object.create(DrawingShape.prototype);
+Bear.prototype.constructor = Bear;
---
+++ 05_Trabalho/js/FotoPrint.js
@@ -0,0 +1,217 @@
+var app = new FotoPrint();
+
+function FotoPrint()
+{
+    this.aImg = new AppImages();
+    this.thingInMotion = null;
+    this.offsetx = null;
+    this.offsetx = null;
+
+    this.init = function() {
+        this.shpinDrawing = new Pool(100);
+        this.canvas1 = document.getElementById('canvas');
+        this.ctx = this.canvas1.getContext("2d");
+
+        this.drawCanvasRect();
+
+        var r1 = new Rect(2, 10, 50, 50,"red",this.canvas1);
+        var s1 = new Rect(60, 10, 50, 50, "green", this.canvas1);
+        var oval1 = new Oval(200, 30, 20, 2.0, 1.0, "teal",this.canvas1);
+        var cir1 = new Oval(300, 30, 20, 1.0, 1.0, "purple",this.canvas1);
+        var pic1 = new Picture(10, 100, 100, 100, this.aImg.dad, this.canvas1);
+        var pic2 = new Picture(120, 100, 100, 100, this.aImg.mom, this.canvas1);
+        var pic3 = new Picture(230, 100, 100, 100, this.aImg.son1, this.canvas1);
+        var pic4 = new Picture(340, 100, 100, 100, this.aImg.son2, this.canvas1);
+        var pic5 = new Picture(450, 100, 100, 100, this.aImg.daughter,this.canvas1);
+        var hrt = new Heart(510, 30, 60, 20, "pink", this.canvas1);
+        var bear = new Bear(10, 250, 100, 100, "black", this.canvas1);
+
+        this.insertObj(pic1);
+        this.insertObj(pic2);
+        this.insertObj(pic3);
+        this.insertObj(pic4);
+        this.insertObj(pic5);
+        this.insertObj(r1);
+        this.insertObj(s1);
+        this.insertObj(oval1);
+        this.insertObj(cir1);
+        this.insertObj(hrt);
+        this.insertObj(bear);
+
+        this.canvas1.addEventListener('mousedown', drag, false);
+        this.canvas1.addEventListener('dblclick', makenewitem, false);
+    };
+
+    this.drawCanvasRect = function() {
+        this.ctx.clearRect(0, 0, this.canvas1.width, this.canvas1.height);
+        this.ctx.strokeStyle = "black";
+        this.ctx.lineWidth = 2;
+        this.ctx.strokeRect(0, 0, this.canvas1.width, this.canvas1.height);
+    };
+
+    this.drawshapes= function() {
+        this.drawCanvasRect();
+        for (var i = 0; i < this.shpinDrawing.stuff.length; i++) {
+            this.shpinDrawing.stuff[i].draw();
+        }
+    };
+
+    this.removeObj = function() {
+        this.shpinDrawing.removeObj();
+        this.drawshapes();
+    };
+
+    this.insertObj = function(obj) {
+        this.shpinDrawing.insertObj(obj);
+        obj.draw();
+    };
+
+    this.clone = function (obj) {
+        var item = new Object();
+        for (var info in obj) {
+            item[info] = obj[info];
+        }
+        return item;
+    };
+}
+
+function AppImages() {
+    this.dad = new Image();
+    this.mom = new Image();
+    this.son1 = new Image();
+    this.son2 = new Image();
+    this.daughter = new Image();
+
+    var numImages = 5;
+    var numLoaded = 0;
+
+    function imageLoaded() {
+        numLoaded++;
+        if (numLoaded === numImages) {
+            app.init();
+        }
+    }
+
+    this.dad.onload = function () {
+        imageLoaded();
+    };
+
+    this.mom.onload = function () {
+        imageLoaded();
+    };
+    this.son1.onload = function () {
+        imageLoaded();
+    };
+    this.son2.onload = function () {
+        imageLoaded();
+    };
+    this.daughter.onload = function () {
+        imageLoaded();
+    };
+    this.dad.src = "imgs/daniel1.jpg";
+    this.mom.src = "imgs/allison1.jpg";
+    this.son1.src = "imgs/grant1.jpg";
+    this.son2.src = "imgs/liam2.jpg";
+    this.daughter.src = "imgs/annika.jpg";
+}
+
+function Pool(maxSize) {
+    var size = maxSize;
+    this.stuff = [];
+
+    this.insertObj = function(obj) {
+        this.stuff.push(obj);
+    };
+
+    this.removeObj = function() {
+        this.stuff.pop();
+    };
+}
+
+
+//Drag & Drop
+function drag(ev) {
+    var mx;
+    var my;
+    if ( ev.layerX ||  ev.layerX == 0) {
+        mx= ev.layerX;
+        my = ev.layerY;
+    } else if (ev.offsetX || ev.offsetX == 0) {
+        mx = ev.offsetX;
+        my = ev.offsetY;
+    }
+    var endpt = app.shpinDrawing.stuff.length-1;
+    for (var i=endpt;i>=0;i--) {
+        if (app.shpinDrawing.stuff[i].overcheck(mx,my)) {
+            app.offsetx = mx-app.shpinDrawing.stuff[i].x;
+            app.offsety = my-app.shpinDrawing.stuff[i].y;
+            var item = app.shpinDrawing.stuff[i];
+            app.thingInMotion = app.shpinDrawing.stuff.length-1;
+            app.shpinDrawing.stuff.splice(i,1);
+            app.shpinDrawing.stuff.push(item);
+            app.canvas1.style.cursor = "pointer";   // change to finger when dragging
+            app.canvas1.addEventListener('mousemove',move,false);
+            app.canvas1.addEventListener('mouseup',drop,false);
+            break;
+        }
+    }
+}
+
+function move(ev) {
+    var mx;
+    var my;
+    if ( ev.layerX ||  ev.layerX == 0) {
+        mx= ev.layerX;
+        my = ev.layerY;
+    } else if (ev.offsetX || ev.offsetX == 0) {
+        mx = ev.offsetX;
+        my = ev.offsetY;
+    }
+    app.shpinDrawing.stuff[app.thingInMotion].x = mx-app.offsetx; //adjust for flypaper dragging
+    app.shpinDrawing.stuff[app.thingInMotion].y = my-app.offsety;
+    app.drawshapes();
+}
+
+function drop(ev) {
+    app.canvas1.removeEventListener('mousemove',move,false);
+    app.canvas1.removeEventListener('mouseup',drop,false);
+    app.canvas1.style.cursor = "crosshair";
+}
+
+//Resposta ao evento doubleClick
+function makenewitem(ev) {
+    var mx;
+    var my;
+    if ( ev.layerX ||  ev.layerX == 0) {
+        mx= ev.layerX;
+        my = ev.layerY;
+    } else if (ev.offsetX || ev.offsetX == 0) {
+        mx = ev.offsetX;
+        my = ev.offsetY;
+    }
+    var endpt = app.shpinDrawing.stuff.length-1;
+    var item;
+    for (var i=endpt;i>=0;i--) {  //reverse order
+        if (app.shpinDrawing.stuff[i].overcheck(mx,my)) {
+            item = app.clone(app.shpinDrawing.stuff[i]);
+            item.x +=20;
+            item.y += 20;
+            app.insertObj(item);
+            break;
+        }
+    }
+}
+
+//Resposta ao evento onclick no botão
+function removeobj() {
+    app.removeObj();
+}
+
+//Resposta ao evento onclick no botão
+function saveasimage() {
+    try {
+        window.open(app.canvas1.toDataURL("imgs/png"));}
+    catch(err) {
+        alert("You need to change browsers OR upload the file to a server.");
+    }
+}
